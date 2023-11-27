09025061284db2405c75a534f1991999200330da
Zeeshan Ahmed <za1423@ic.ac.uk>
2023-11-27 19:19:59 +0000

    query test

--- api/app.py
+++ api/app.py
@@ -18,3 +18,10 @@ def submit():
     )
 
 
+def process_query(query):
+    if (query == "dinosaurs"):
+        return "Dinosaurs ruled the Earth 200 million years ago"
+
+    if (query == "asteroids"):
+        return "Unknown"
+

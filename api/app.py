f0a00fedc95a0f8cda51e205b3e3b4278603e21e
Zeeshan Ahmed <za1423@ic.ac.uk>
2023-11-27 19:00:57 +0000

    added form and hello.html

--- api/app.py
+++ api/app.py
@@ -8,10 +8,11 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route("/submit", methods = ["POST"])
+@app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_email = request.form.get("email")
     input_message = request.form.get("message")
-    return render_template("hello.html", name=input_name, email=input_email, message=input_message)
-    
+    return render_template(
+        "hello.html", name=input_name, email=input_email, message=input_message
+    )
